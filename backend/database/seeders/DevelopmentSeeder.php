2072e9a7da4a4de7c117d73a7c15736a748582fa
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2026-03-07 10:13:30 -0300

    be - seeder: improve development seeder

--- backend/database/seeders/DevelopmentSeeder.php
+++ backend/database/seeders/DevelopmentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Friendship;
 use Illuminate\Database\Seeder;
 
 use App\Models\User;
@@ -14,11 +15,27 @@ class DevelopmentSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        $users[] = User::factory()->create([
             'name' => 'Juninho',
             'username' => 'junin_99',
             'email' => 'juninho@email.com',
             'password' => Hash::make('juninho99'),
         ]);
+
+        $users[] = User::factory()->create([
+            'name' => 'Jão',
+            'username' => 'jao_99',
+            'email' => 'jao@email.com',
+            'password' => Hash::make('jaozin99'),
+        ]);
+
+        $users[] = User::factory()->create([
+            'name' => 'Júlia',
+            'username' => 'julia_99',
+            'email' => 'julia@email.com',
+            'password' => Hash::make('julia99'),
+        ]);
+
+        Friendship::makeFriends($users[0]->id, $users[1]->id);
     }
 }
