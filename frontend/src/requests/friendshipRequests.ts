a5feb8adc02b39f2925d2711bb84f3bd2805ba4f
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2026-03-20 17:34:39 -0300

    fe - feat: add remove friendship && sendFrienship requests

--- frontend/src/requests/friendshipRequests.ts
+++ frontend/src/requests/friendshipRequests.ts
@@ -8,6 +8,7 @@ import {
 import { requester } from "./config";
 
 const FRIENDSHIP_ROUTE = "friendships";
+const FRIENDSHIP_REQUEST_ROUTE = "friendship-requests";
 
 export async function checkFriendship(
   id: number
@@ -18,5 +19,15 @@ export async function checkFriendship(
 export async function requestFriendship(
   data: RequestFriendshipModel
 ): Promise<AxiosResponse<FriendshipRequestModel>> {
-  return await requester().post(`${FRIENDSHIP_ROUTE}/request`, data);
+  return await requester().post(FRIENDSHIP_REQUEST_ROUTE, data);
+}
+
+export function sentFriendship(): Promise<
+  AxiosResponse<FriendshipRequestModel[]>
+> {
+  return requester().get(`${FRIENDSHIP_REQUEST_ROUTE}/sent`);
+}
+
+export function removeFriendship(id: number): Promise<AxiosResponse<void>> {
+  return requester().delete(`${FRIENDSHIP_REQUEST_ROUTE}/${id}`);
 }
