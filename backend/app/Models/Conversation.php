5100f1906d81b5baa179bc9efb46062de647bab0
Jhonatan de Azevedo da Silva <jhonatanazevedo99@gmail.com>
2026-05-17 11:26:11 -0300

    be - conversation: order conversations by id DESC

--- backend/app/Models/Conversation.php
+++ backend/app/Models/Conversation.php
@@ -27,6 +27,6 @@ class Conversation extends Model
 
     public function messages()
     {
-        return $this->hasMany(Message::class);
+        return $this->hasMany(Message::class)->orderBy('id', 'DESC');
     }
 }
